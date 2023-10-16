ec6b690950a7bdba9c10571a20c7c427b640be42
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-10-16 21:21:11 +0530

    Add some more tests to really close the final gap in coverage percentage
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- test/test_tkts.py
+++ test/test_tkts.py
@@ -117,6 +117,62 @@ from protop2g.main import main
             id="Transferring issue tickets with SHUT status along with states but without comments and without labels",  # noqa: E501
         ),
         pytest.param(
+            f"--srce {envr['TEST_SRCE']} --dest {envr['TEST_DEST']} --pkey {envr['TEST_PKEY']} --gkey {envr['TEST_GKEY']} --fusr {envr['TEST_FUSR']} --tusr {envr['TEST_TUSR']} tkts --status OPEN --select 1 --commit --comments",  # noqa: E501
+            0,
+            [
+                "[ WARN ] Extracting all open issue tickets without labels, with states and with comments off the given selection",  # noqa: E501
+                "[ BUSY ] Probing issue ticket #1...",
+                "Information retrieved in ",
+                "[ BUSY ] Migrating issue ticket without labels #1 'This is the title of the first test issue' by 'Akashdeep Dhar (ID t0xic0der)'...",  # noqa: E501
+                "Migrated to",
+                "[ BUSY ] Asserting issue ticket status...",
+                "Assertion unnecessary due to the OPEN status of the ticket",
+                "[ BUSY ] Reading comment information...",
+                "Found 3 entities in ",
+                "[ BUSY ] Transferring comment (Entity 1 of 3)...",
+                "Transferred to ",
+                "[ BUSY ] Transferring comment (Entity 2 of 3)...",
+                "Transferred to ",
+                "[ BUSY ] Transferring comment (Entity 3 of 3)...",
+                "Transferred to ",
+            ],
+            id="Transferring particular issue tickets with OPEN status along with states and comments but without labels",  # noqa: E501
+        ),
+        pytest.param(
+            f"--srce {envr['TEST_SRCE']} --dest {envr['TEST_DEST']} --pkey {envr['TEST_PKEY']} --gkey {envr['TEST_GKEY']} --fusr {envr['TEST_FUSR']} --tusr {envr['TEST_TUSR']} tkts --status SHUT --select 2 --commit --comments",  # noqa: E501
+            0,
+            [
+                "[ WARN ] Extracting all closed issue tickets without labels, with states and with comments off the given selection",  # noqa: E501
+                "[ BUSY ] Probing issue ticket #2...",
+                "Information retrieved in ",
+                "[ BUSY ] Migrating issue ticket without labels #2 'This is the title of the second test issue' by 'Akashdeep Dhar (ID t0xic0der)'...",  # noqa: E501
+                "Migrated to",
+                "[ BUSY ] Asserting issue ticket status...",
+                "Asserted CLOSE status of the ticket in ",
+                "[ BUSY ] Reading comment information...",
+                "Found 4 entities in ",
+                "[ BUSY ] Transferring comment (Entity 1 of 4)...",
+                "Transferred to ",
+                "[ BUSY ] Transferring comment (Entity 2 of 4)...",
+                "Transferred to ",
+                "[ BUSY ] Transferring comment (Entity 3 of 4)...",
+                "Transferred to ",
+                "[ BUSY ] Transferring comment (Entity 4 of 4)...",
+                "Transferred to ",
+            ],
+            id="Transferring particular issue tickets with SHUT status along with states and comments but without labels",  # noqa: E501
+        ),
+        pytest.param(
+            f"--srce {envr['TEST_SRCE']} --dest {envr['TEST_DEST']} --pkey {envr['TEST_PKEY']} --gkey {envr['TEST_GKEY']} --fusr {envr['TEST_FUSR']} --tusr {envr['TEST_TUSR']} tkts --status SHUT --select 1",  # noqa: E501
+            0,
+            [
+                "[ WARN ] Extracting all closed issue tickets without labels, without states and without comments off the given selection",  # noqa: E501
+                "[ BUSY ] Probing issue ticket #1...",
+                "Skipping issue ticket as the issue ticket status does not match the provided status",  # noqa: E501
+            ],
+            id="Transferring particular issue tickets with OPEN status without labels, without states and without comments when SHUT status is prescribed",  # noqa: E501
+        ),
+        pytest.param(
             f"--srce {envr['TEST_SRCE']} --dest {envr['TEST_DEST']} --pkey {envr['TEST_PKEY']} --gkey {envr['TEST_GKEY']} --fusr {envr['TEST_FUSR']} --tusr {envr['TEST_TUSR']} tkts --status FULL --commit",  # noqa: E501
             0,
             [

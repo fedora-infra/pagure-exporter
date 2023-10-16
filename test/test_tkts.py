12ba1cb5715d1285f156ff33445a6e72a43434d6
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-10-16 15:20:58 +0530

    Make minimal changes and add license excerpt on the top
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- test/test_tkts.py
+++ test/test_tkts.py
@@ -1,3 +1,27 @@
+"""
+protop2g
+Copyright (C) 2022-2023 Akashdeep Dhar
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Any Red Hat trademarks that are incorporated in the source
+code or documentation are not subject to the GNU General Public
+License and may only be used or replicated with the express permission
+of Red Hat, Inc.
+"""
+
+
 from os import environ as envr
 
 import pytest
@@ -308,7 +332,6 @@ from protop2g.main import main
 def test_main_tkts(caplog, cmdl, code, text):
     runner = CliRunner()
     result = runner.invoke(main, cmdl)
-    print(caplog.text)
     assert result.exit_code == code  # noqa: S101
     for indx in text:
         assert indx in caplog.text  # noqa: S101
